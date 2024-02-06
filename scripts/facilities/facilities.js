859f9edd9adaba8e31add3b720002e46f6d5d2d1
Joshua Hamrick <hamrick.joshuac@gmail.com>
2024-02-06 13:54:49 -0600

    added facility drop down

---
+++ scripts/facilities/facilities.js
@@ -0,0 +1,20 @@
+import { getFacilities } from "./facilityData.js"
+
+const facilities = getFacilities()
+
+const facilitiesHTML = ()  => {
+    let facilitiesString = `<div class="facilities">
+                            <label for="facility-names" class="facility-label">Choose a facility</label>
+                            <select name="facility-names" class="facilities--facilities-dropdown">`
+    for (const facility of facilities) {
+        if (facility.active == true) {
+            facilitiesString += `<option value="${facility.name}" class="facilities--option">
+                                ${facility.name}</option>`
+            }
+    }
+    facilitiesString +=`</select>
+                        </div>`
+    return facilitiesString
+}
+
+
