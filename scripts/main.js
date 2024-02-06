4aba2e209bd845f31217cc4740d88b569a293bc5
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-02-06 14:58:02 -0600

    added await

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import { getFacilitiesHTML } from "./facilities/facilities.js"
-const facilitiesHTML = getFacilitiesHTML()
+
+
+const facilitiesHTML = await getFacilitiesHTML()
 const dropdownElement = document.querySelector(".dropdowns")
 dropdownElement.innerHTML = facilitiesHTML  
 
