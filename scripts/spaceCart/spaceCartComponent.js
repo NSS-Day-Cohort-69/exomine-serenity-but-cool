5c5567acb86aa3d43b69cdc2143304c39a994c76
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-02-07 11:15:48 -0600

    changes

--- scripts/spaceCart/spaceCartComponent.js
+++ scripts/spaceCart/spaceCartComponent.js
@@ -14,19 +14,19 @@ export const getSpaceCartHTML = async () => {
     //get facilityMinerals
     //get planets
 
-    //get current facilityMineral from transaction
+    //get current facilityMineralId from transaction
     //get current planet from transaction
 
     //find facilityMineral instance in facilityMinerals
     //find planet instance in planets
 
-    const facilityMineral = { id: 1, facilityId: 1, mineralId: 1, mineralTons: 100 }
+    const facilityMineral = { id: 1, facilityId: 1, mineralId: 1, mineralTons: 100, mineral: {id: 1, name: "Gigglium"} }
     const currentPlanet = { id: 1, name: "Laughalotia" }
 
     let spaceCartHTML = `<div class="space-cart">
      <h3 class="space-cart--header">Space Cart</h3>`
     if (facilityMineral != null) {
-        spaceCartHTML += `<p> 1 ton of ${facilityMineral} from ${currentPlanet}</p>`
+        spaceCartHTML += `<p> 1 ton of ${facilityMineral.mineral.name} from ${currentPlanet.name}</p>`
         // spaceCartMineralHTML()
     }
 
