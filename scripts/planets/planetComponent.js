5d41b6524badb63f52bb21861aded5350cac091e
Joshua Hamrick <hamrick.joshuac@gmail.com>
2024-02-08 14:18:40 -0600

    Requested changes

--- scripts/planets/planetComponent.js
+++ scripts/planets/planetComponent.js
@@ -7,29 +7,31 @@ const planets = await getPlanets()
 const minerals = await getMinerals()
 
 export const getPlanetHtml = async () => {
-    
     const planetMineral = getPlanetMineral()
-    const planetMineralsData = await getPlanetMinerals()
-    let planetsHtml = `<div class="planet">`
-    for (const planet of planets) {
-        if (planet.id == planetMineral.planetId) {
-            planetsHtml += `<h2 class="planet--name">${planet.name} Minerals</h2>
-                            <ul class="planet--mineralsDiv">`
-            for (const planetMinerals of planetMineralsData) {
-                if (planetMinerals.planetId == planet.id) {
-                    for (const mineral of minerals) {
-                        if (planetMinerals.mineralId == mineral.id) {
-                            planetsHtml += `<li class="planet--mineral">${planetMinerals.mineralTons} tons ${mineral.name}</li>`
+    if (planetMineral !== null) {
+        const planetMineralsData = await getPlanetMinerals()
+        let planetsHtml = `<div class="planet">`
+        for (const planet of planets) {
+            if (planet.id == planetMineral.planetId) {
+                planetsHtml += `<h2 class="planet--name">${planet.name} Minerals</h2>
+                                <ul class="planet--mineralsDiv">`
+                for (const planetMinerals of planetMineralsData) {
+                    if (planetMinerals.planetId == planet.id) {
+                        for (const mineral of minerals) {
+                            if (planetMinerals.mineralId == mineral.id) {
+                                planetsHtml += `<li class="planet--mineral">${planetMinerals.mineralTons} tons ${mineral.name}</li>`
+                            }
                         }
                     }
                 }
             }
         }
-    }
-
     
-
-    planetsHtml += `</ul>
-                </div>`
-    return planetsHtml
+        
+    
+        planetsHtml += `</ul>
+                    </div>`
+        return planetsHtml
+        
+    }
 }
