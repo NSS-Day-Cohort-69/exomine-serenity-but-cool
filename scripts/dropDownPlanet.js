22557e06e071c1e14e3769591170ec211a5e00ef
Ruby Kendrick <kendrick.ruby@gmail.com>
2024-02-08 11:41:32 -0600

    planet dropdown component tested

--- scripts/dropDownPlanet.js
+++ scripts/dropDownPlanet.js
@@ -3,8 +3,9 @@ import { getPlanetHTML } from "../planets/planetComponent.js"
 
 export const getPlanetDropDownHTML = async () => {
     const dropdownHTML = await getDropDownHTML()
-    const planetHTML = await getFacilitiesHTML()
-    const planetDropDownHTML = `<div class="dropdownsPlanet">
+    const planetHTML = await getPlanetHTML()
+    //const placeHolder = "does this work bruh"
+    const planetDropDownHTML = `<div class="dropdowns-planet">
                             ${dropdownHTML}
                             ${planetHTML}
                          </div>`
--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ import { getGovernorsHTML } from "./governors/governorComponent.js"
 import { getSpaceCartHTML } from "./spaceCart/spaceCartComponent.js"
 import { getDropDownHTML } from "./dropDowns.js"
 import { doTransaction, updateFacilityMineral, updatePlanetMineral } from "./transaction.js"
+import { getPlanetDropDownHTML } from "./dropDownPlanet.js"
 
 const render = async () =>
 {
@@ -54,3 +55,5 @@ const render = async () =>
 render()
 
 document.addEventListener("domUpdated", render)
+
+console.log(await getPlanetDropDownHTML())
